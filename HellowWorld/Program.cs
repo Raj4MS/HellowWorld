a6fc5ae0e5c3c2eb050a63daac96e887b1e4f993
Raj kumar <rajkumar4543@gmail.com>
2018-01-27 23:57:45 +0530

    init

---
+++ HellowWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.IO;
+
+namespace HellowWorld
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            File.WriteAllText("D:\\hello.txt", "Raj kumar");
+        }
+    }
+}
